44f19cccbb38a7f2b75ab754bd30b07c539334b9
Marc Valsells <marc@valsells.me>
2021-03-08 16:18:01 +0100

    Initial commit

---
+++ SDM-P2-FaseA.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=SDM-P2-FaseA.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/SDM-P2-FaseA.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=sdm-p2-fasea.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/sdm-p2-fasea.x.tar
