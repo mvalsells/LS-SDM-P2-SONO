e347465db154b4c64f6f4dd93f59355de3548878
Marc Valsells <marc@valsells.me>
2021-03-16 16:28:18 +0100

    Prova fallida

--- SDM-P2-FaseA.X/P2_FA_pic_8b_general.asm
+++ SDM-P2-FaseA.X/P2_FA_pic_8b_general.asm
@@ -140,6 +140,7 @@ LOOP
     ;codi
     btfsc PIR1,RCIF,0
     goto LECTOR_EUSART
+    call LLEGIR_RX
     goto LOOP
 ;-------------------------------------------------------------------------------
 HIGH_RSI
@@ -199,9 +200,10 @@ ESPERA_TX
 LLEGIR_RX
     btfss PIR1,RCIF,0
     goto LLEGIR_RX
-    movf RCREG,0,0
-    movwf eusart_input,0
-    movff eusart_input,TXREG
+    ;movf RCREG,0,0
+    ;movwf eusart_input,0
+    ;movff eusart_input,TXREG
+    movff RCREG,TXREG
     call ESPERA_TX
     return
     
